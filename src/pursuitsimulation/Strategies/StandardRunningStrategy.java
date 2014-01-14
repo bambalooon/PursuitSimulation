ae7a0c01d89cc5212093b432127a88af9dd41a30
bambaloon <mike.kurzeja@gmail.com>
2014-01-14 19:39:26 +0100

    added .gitignore added vector class

--- src/pursuitsimulation/Strategies/StandardRunningStrategy.java
+++ src/pursuitsimulation/Strategies/StandardRunningStrategy.java
@@ -1,14 +1,17 @@
 package pursuitsimulation.Strategies;
 
 import pursuitsimulation.Crossing;
+import pursuitsimulation.People.Catcher;
 import pursuitsimulation.People.Person;
 import pursuitsimulation.People.Runner;
 import pursuitsimulation.Simulation.SimulationProcess;
 import pursuitsimulation.Simulation.SimulationProgram;
 import pursuitsimulation.Strategies.RunningStrategy;
 import pursuitsimulation.Vertex;
+import pursuitsimulation.util.Vector;
 
 import java.util.LinkedList;
+import java.util.ListIterator;
 import java.util.Random;
 
 /**
@@ -22,7 +25,11 @@ public class StandardRunningStrategy extends RunningStrategy {
     public StandardRunningStrategy(SimulationProcess process) {
         super(process);
     }
-    public Crossing getDestination(Person r) {
+    public Crossing getDestination(Person p) {
+        Runner r = (Runner) p;
+
+        findEscapeNode(r);
+
         Crossing v = r.getCurr();
         LinkedList<Vertex> nhood = v.getNeighbours();
 
@@ -35,5 +42,40 @@ public class StandardRunningStrategy extends RunningStrategy {
             return (Crossing) nhood.get(0);
         }
         return (Crossing) (nhood.get(SimulationProgram.randomGenerator.nextInt(nhood.size() - 1)));
+    }
+
+    public void findEscapeNode(Runner r) {
+        Vector shortest, v = new Vector();
+        ListIterator<Catcher> it = process.getCatchers().listIterator();
+
+        while(it.hasNext()) {
+            v.add( computeVectorToCatcher( it.next(), r ) );
+        }
+
+        shortest = findVectorToClosestCatcher(r);
+
+        v.negate().add( shortest.negate() );
+
+        System.out.println("Escape vector: " + v);
+    }
+
+    public Vector findVectorToClosestCatcher(Runner r) {
+        ListIterator<Catcher> it = process.getCatchers().listIterator();
+
+        Vector v, shortest = computeVectorToCatcher( it.next(), r );
+
+        while(it.hasNext()) {
+            v = computeVectorToCatcher( it.next(), r );
+
+            if(v.getLength() < shortest.getLength())
+                shortest = v;
+        }
+
+        return shortest;
+    }
+
+    public Vector computeVectorToCatcher(Catcher c, Runner r) {
+        /* Person::getVector returns coordinates of a person packed in Vector class for convinience */
+        return c.getVector().add( r.getVector().negate() );
     }
 }
---
+++ src/pursuitsimulation/util/Vector.java
@@ -0,0 +1,71 @@
+package pursuitsimulation.util;
+
+import sun.management.resources.agent_pt_BR;
+
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+
+/**
+ * Created by mike on 1/14/14.
+ */
+public class Vector {
+    private double x;
+    private double y;
+
+    public Vector() {
+        x = 0;
+        y = 0;
+    }
+
+    public Vector(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Vector(Vector v) {
+        x = v.x;
+        y = v.y;
+    }
+
+    public Vector add(Vector v) {
+        x += v.x;
+        y += v.y;
+
+        return this;
+    }
+
+    public Vector negate() {
+        x = -x;
+        y = -x;
+
+        return this;
+    }
+
+    public double getLength() {
+        return sqrt(pow((x),2) + pow((y), 2));
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public Vector setX(double x) {
+        this.x = x;
+
+        return this;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public Vector setY(double y) {
+        this.y = y;
+
+        return this;
+    }
+
+    public String toString() {
+        return "[" + x + ", " + y + "]";
+    }
+}

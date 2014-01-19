b06d037cf7a02dcbc4f900eab1742b2beb6a90b3
bambalooon <mike.kurzeja@gmail.com>
2014-01-19 14:33:22 +0100

    added missing exception

---
+++ src/pursuitsimulation/Exceptions/NoPlayerException.java
@@ -0,0 +1,12 @@
+package pursuitsimulation.Exceptions;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: BamBalooon
+ * Date: 18.01.14
+ * Time: 19:55
+ * To change this template use File | Settings | File Templates.
+ */
+public class NoPlayerException extends Exception {
+
+}
